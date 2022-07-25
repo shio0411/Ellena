9f863e57f91b44c87f9ea046ec1aa5ef43ccef39
duylvlse160831 <duylvlse160831@fpt.edu.vn>
2022-07-25 16:19:08 +0700

    remove redundant import

--- src/java/store/controllers/UpdateTrackingIdController.java
+++ src/java/store/controllers/UpdateTrackingIdController.java
@@ -6,8 +6,6 @@
 package store.controllers;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
